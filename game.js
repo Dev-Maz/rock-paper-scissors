1af510bc6e643d7fcd5a1d7b737b4415d0b7827e
Maziar-M <maziar.mir.2002@gmail.com>
2023-07-14 11:56:57 +0330

    Add getPlayerChoice() to game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    switch(randomNumber) {
+    switch (randomNumber) {
         case 0:
             return "Rock";
             break;
@@ -10,5 +10,20 @@ function getComputerChoice() {
         case 2:
             return "Scissors";
             break;
+    }
+}
+
+function getPlayerChoice() {
+    let choice = prompt("Type: R for Rock, P for Paper, S for Scissors");
+    if (choice === null) {
+        return "Canceled";
+    } else if (choice.toLowerCase() === "r") {
+        return "Rock";
+    } else if (choice.toLowerCase() === "p") {
+        return "Paper";
+    } else if (choice.toLowerCase() === "s") {
+        return "Scissors";
+    } else {
+        return "Wrong Choice";
     }
 }
