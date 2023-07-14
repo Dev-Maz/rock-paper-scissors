fe39b98f86bb3b5d02e27f27704f681d858796bd
Maziar-M <maziar.mir.2002@gmail.com>
2023-07-14 13:12:06 +0330

    Add playRound() to game.js

--- game.js
+++ game.js
@@ -27,3 +27,55 @@ function getPlayerChoice() {
         return "Wrong Choice";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Canceled") {
+        return "Canceled";
+    } else if (playerSelection === "Wrong Choice") {
+        return "Wrong Choice";
+    }
+
+    switch (playerSelection) {
+        case "Rock":
+            switch (computerSelection) {
+                case "Rock":
+                    return "Tie!";
+                    break;
+                case "Paper":
+                    return `You lose! ${computerSelection} beats ${playerSelection}`;
+                    break;
+                case "Scissors":
+                    return `You win! ${playerSelection} beats ${computerSelection}`;
+                    break;
+            }
+            break;
+
+        case "Paper":
+            switch (computerSelection) {
+                case "Rock":
+                    return `You win! ${playerSelection} beats ${computerSelection}`;
+                    break;
+                case "Paper":
+                    return "Tie!";
+                    break;
+                case "Scissors":
+                    return `You lose! ${computerSelection} beats ${playerSelection}`;
+                    break;
+            }
+            break;
+
+        case "Scissors":
+            switch (computerSelection) {
+                case "Rock":
+                    return `You lose! ${computerSelection} beats ${playerSelection}`;
+                    break;
+                case "Paper":
+                    return `You win! ${playerSelection} beats ${computerSelection}`;
+                    break;
+                case "Scissors":
+                    return "Tie!";
+                    break;
+            }
+            break;
+    }
+}
